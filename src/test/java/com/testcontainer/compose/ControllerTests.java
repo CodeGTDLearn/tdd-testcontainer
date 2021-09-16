PauloPortfolio/tdd-testcontainer-playground

5.8) Refactored Tests
*CONCLUDED:
a) Folder Tests Structure

*WORKING ON:

src/test/java/com/testcontainer/compose/ControllerTests.java
@@ -3,6 +3,8 @@ package com.testcontainer.compose;
 import com.github.javafaker.Faker;
 import com.testcontainer.api.entity.Customer;
 import com.testcontainer.api.service.ICustomerService;
+import com.testcontainer.compose.config.ControllerConfig;
+import com.testcontainer.compose.config.ModelConfig;
 import io.restassured.http.ContentType;
 import io.restassured.module.webtestclient.RestAssuredWebTestClient;
 import org.junit.jupiter.api.*;
@@ -31,7 +33,7 @@ import static org.hamcrest.CoreMatchers.hasItem;
 import static org.hamcrest.Matchers.is;
 import static org.springframework.http.HttpStatus.*;
 
-public class ControllerTests extends ConfigControllerCompose {
+public class ControllerTests extends ControllerConfig {
 
     private List<Customer> customerList;
     private Customer customerWithId;
@@ -39,7 +41,7 @@ public class ControllerTests extends ConfigControllerCompose {
 
 
     @Container
-    private static final DockerComposeContainer<?> compose = new ConfigTests().compose;
+    private static final DockerComposeContainer<?> compose = new ModelConfig().compose;
 
 
     // MOCKED-SERVER: WEB-TEST-CLIENT(non-blocking client)'
@@ -55,13 +57,13 @@ public class ControllerTests extends ConfigControllerCompose {
 
     @BeforeAll
     static void beforeAll() {
-        ConfigTests.beforeAll();
+        ModelConfig.beforeAll();
     }
 
 
     @AfterAll
     static void afterAll() {
-        ConfigTests.afterAll();
+        ModelConfig.afterAll();
         compose.close();
     }
 
@@ -109,10 +111,10 @@ public class ControllerTests extends ConfigControllerCompose {
     @Test
     void checkServices() {
 
-        new ConfigTests().checkTestcontainerComposeService(
+        new ModelConfig().checkTestcontainerComposeService(
              compose,
-             ConfigTests.SERVICE,
-             ConfigTests.SERVICE_PORT
+             ModelConfig.SERVICE,
+             ModelConfig.SERVICE_PORT
                                                           );
     }
 

src/test/java/com/testcontainer/compose/RepoTests.java
@@ -2,6 +2,7 @@ package com.testcontainer.compose;
 
 import com.testcontainer.api.entity.Customer;
 import com.testcontainer.api.repository.IRepository;
+import com.testcontainer.compose.config.ModelConfig;
 import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.testcontainers.containers.DockerComposeContainer;
@@ -20,7 +21,7 @@ import java.util.concurrent.TimeoutException;
 
 import static com.testcontainer.databuilder.CustomerBuilder.customerWithName;
 
-public class RepoTests extends ConfigTests {
+public class RepoTests extends ModelConfig {
 
     private Customer customer1;
     private Customer customer2;
@@ -29,7 +30,7 @@ public class RepoTests extends ConfigTests {
 
 
     @Container
-    private static final DockerComposeContainer<?> compose = new ConfigTests().compose;
+    private static final DockerComposeContainer<?> compose = new ModelConfig().compose;
 
 
     @Autowired
@@ -37,14 +38,14 @@ public class RepoTests extends ConfigTests {
 
 
     @BeforeAll
-    static void beforeAll() {
-        ConfigTests.beforeAll();
+    static void setupAll() {
+        ModelConfig.beforeAll();
     }
 
 
     @AfterAll
-    static void afterAll() {
-        ConfigTests.afterAll();
+    public static void afterAll() {
+        ModelConfig.afterAll();
         compose.close();
     }
 
@@ -73,8 +74,8 @@ public class RepoTests extends ConfigTests {
     public void checkServices() {
         super.checkTestcontainerComposeService(
              compose,
-             ConfigTests.SERVICE,
-             ConfigTests.SERVICE_PORT
+             ModelConfig.SERVICE,
+             ModelConfig.SERVICE_PORT
                                               );
     }
 

src/test/java/com/testcontainer/compose/ServiceTests.java
@@ -4,6 +4,7 @@ import com.testcontainer.api.entity.Customer;
 import com.testcontainer.api.service.CustomerService;
 import com.testcontainer.api.repository.IRepository;
 import com.testcontainer.api.service.ICustomerService;
+import com.testcontainer.compose.config.ModelConfig;
 import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.testcontainers.containers.DockerComposeContainer;
@@ -23,7 +24,7 @@ import java.util.concurrent.TimeoutException;
 
 import static com.testcontainer.databuilder.CustomerBuilder.customerWithName;
 
-public class ServiceTests extends ConfigTests {
+public class ServiceTests extends ModelConfig {
 
   private Customer customer1;
   private Customer customer2;
@@ -31,7 +32,7 @@ public class ServiceTests extends ConfigTests {
   private List<Customer> customerList;
 
   @Container
-  private static final DockerComposeContainer<?> compose = new ConfigTests().compose;
+  private static final DockerComposeContainer<?> compose = new ModelConfig().compose;
 
 
   @Autowired
@@ -41,14 +42,14 @@ public class ServiceTests extends ConfigTests {
 
 
   @BeforeAll
-  static void beforeAll() {
-    ConfigTests.beforeAll();
+  static void setUpAll() {
+    ModelConfig.beforeAll();
   }
 
 
   @AfterAll
-  static void afterAll() {
-    ConfigTests.afterAll();
+  static void tearDownAll() {
+    ModelConfig.afterAll();
     compose.close();
   }
 
@@ -84,8 +85,8 @@ public class ServiceTests extends ConfigTests {
 
     super.checkTestcontainerComposeService(
          compose,
-         ConfigTests.SERVICE,
-         ConfigTests.SERVICE_PORT
+         ModelConfig.SERVICE,
+         ModelConfig.SERVICE_PORT
                                           );
   }
 

src/test/java/com/testcontainer/container/ControllerTests.java
@@ -3,6 +3,8 @@ package com.testcontainer.container;
 import com.github.javafaker.Faker;
 import com.testcontainer.api.entity.Customer;
 import com.testcontainer.api.service.ICustomerService;
+import com.testcontainer.container.config.ModelConfig;
+import com.testcontainer.container.config.ControllerConfig;
 import io.restassured.http.ContentType;
 import io.restassured.module.webtestclient.RestAssuredWebTestClient;
 import org.junit.jupiter.api.*;
@@ -33,9 +35,9 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
 import static org.springframework.http.HttpStatus.*;
 
-@SpringBootTest(webEnvironment = RANDOM_PORT)
-@AutoConfigureWebTestClient
-public class ControllerTests extends ContainerConfig {
+//@SpringBootTest(webEnvironment = RANDOM_PORT)
+//@AutoConfigureWebTestClient
+public class ControllerTests extends ControllerConfig {
 
   final private String enabledTest = "true";
   private List<Customer> customerList;
@@ -55,13 +57,13 @@ public class ControllerTests extends ContainerConfig {
 
   @BeforeAll
   public static void beforeAll() {
-    ConfigTests.beforeAll();
+    ModelConfig.beforeAll();
   }
 
 
   @AfterAll
   public static void afterAll() {
-    ConfigTests.afterAll();
+    ModelConfig.afterAll();
   }
 
 

src/test/java/com/testcontainer/container/RepoTests.java
@@ -2,6 +2,7 @@ package com.testcontainer.container;
 
 import com.testcontainer.api.entity.Customer;
 import com.testcontainer.api.repository.IRepository;
+import com.testcontainer.container.config.ModelConfig;
 import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Lazy;
@@ -22,7 +23,7 @@ import java.util.concurrent.TimeoutException;
 import static com.testcontainer.databuilder.CustomerBuilder.customerWithName;
 import static org.junit.jupiter.api.Assertions.*;
 
-public class RepoTests extends ConfigTests {
+public class RepoTests extends ModelConfig {
 
   final private String enabledTest = "true";
   private List<Customer> customerList;
@@ -35,13 +36,13 @@ public class RepoTests extends ConfigTests {
 
   @BeforeAll
   public static void beforeAll() {
-    ConfigTests.beforeAll();
+    ModelConfig.beforeAll();
   }
 
 
   @AfterAll
   public static void afterAll() {
-    ConfigTests.afterAll();
+    ModelConfig.afterAll();
   }
 
 

src/test/java/com/testcontainer/container/ServiceTests.java
@@ -4,6 +4,7 @@ import com.testcontainer.api.entity.Customer;
 import com.testcontainer.api.service.CustomerService;
 import com.testcontainer.api.repository.IRepository;
 import com.testcontainer.api.service.ICustomerService;
+import com.testcontainer.container.config.ModelConfig;
 import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Lazy;
@@ -25,7 +26,7 @@ import static com.testcontainer.databuilder.CustomerBuilder.customerWithName;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 // https://www.baeldung.com/spring-data-mongodb-reactive
-public class ServiceTests extends ConfigTests {
+public class ServiceTests extends ModelConfig {
 
   final private String enabledTest = "true";
   private List<Customer> customerList;
@@ -40,13 +41,13 @@ public class ServiceTests extends ConfigTests {
 
   @BeforeAll
   public static void beforeAll() {
-    ConfigTests.beforeAll();
+    ModelConfig.beforeAll();
   }
 
 
   @AfterAll
   public static void afterAll() {
-    ConfigTests.afterAll();
+    ModelConfig.afterAll();
   }
 
 

CodeGTDLearn/tdd-testcontainer

src/test/java/com/testcontainer/compose/ControllerTests.java
@@ -3,6 +3,8 @@ package com.testcontainer.compose;
 import com.github.javafaker.Faker;
 import com.testcontainer.api.entity.Customer;
 import com.testcontainer.api.service.ICustomerService;
+import com.testcontainer.compose.config.ControllerConfig;
+import com.testcontainer.compose.config.Config;
 import io.restassured.http.ContentType;
 import io.restassured.module.webtestclient.RestAssuredWebTestClient;
 import org.junit.jupiter.api.*;
@@ -31,7 +33,7 @@ import static org.hamcrest.CoreMatchers.hasItem;
 import static org.hamcrest.Matchers.is;
 import static org.springframework.http.HttpStatus.*;
 
-public class ControllerTests extends ConfigControllerCompose {
+public class ControllerTests extends ControllerConfig {
 
     private List<Customer> customerList;
     private Customer customerWithId;
@@ -39,7 +41,7 @@ public class ControllerTests extends ConfigControllerCompose {
 
 
     @Container
-    private static final DockerComposeContainer<?> compose = new ConfigTests().compose;
+    private static final DockerComposeContainer<?> compose = new Config().compose;
 
 
     // MOCKED-SERVER: WEB-TEST-CLIENT(non-blocking client)'
@@ -55,13 +57,13 @@ public class ControllerTests extends ConfigControllerCompose {
 
     @BeforeAll
     static void beforeAll() {
-        ConfigTests.beforeAll();
+        Config.beforeAll();
     }
 
 
     @AfterAll
     static void afterAll() {
-        ConfigTests.afterAll();
+        Config.afterAll();
         compose.close();
     }
 
@@ -109,11 +111,11 @@ public class ControllerTests extends ConfigControllerCompose {
     @Test
     void checkServices() {
 
-        new ConfigTests().checkTestcontainerComposeService(
+        new Config().checkTestcontainerComposeService(
              compose,
-             ConfigTests.SERVICE,
-             ConfigTests.SERVICE_PORT
-                                                          );
+             Config.SERVICE,
+             Config.SERVICE_PORT
+                                                     );
     }
 
 

src/test/java/com/testcontainer/compose/RepoTests.java
@@ -2,6 +2,7 @@ package com.testcontainer.compose;
 
 import com.testcontainer.api.entity.Customer;
 import com.testcontainer.api.repository.IRepository;
+import com.testcontainer.compose.config.Config;
 import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.testcontainers.containers.DockerComposeContainer;
@@ -20,7 +21,7 @@ import java.util.concurrent.TimeoutException;
 
 import static com.testcontainer.databuilder.CustomerBuilder.customerWithName;
 
-public class RepoTests extends ConfigTests {
+public class RepoTests extends Config {
 
     private Customer customer1;
     private Customer customer2;
@@ -29,7 +30,7 @@ public class RepoTests extends ConfigTests {
 
 
     @Container
-    private static final DockerComposeContainer<?> compose = new ConfigTests().compose;
+    private static final DockerComposeContainer<?> compose = new Config().compose;
 
 
     @Autowired
@@ -37,14 +38,14 @@ public class RepoTests extends ConfigTests {
 
 
     @BeforeAll
-    static void beforeAll() {
-        ConfigTests.beforeAll();
+    static void setupAll() {
+        Config.beforeAll();
     }
 
 
     @AfterAll
-    static void afterAll() {
-        ConfigTests.afterAll();
+    public static void afterAll() {
+        Config.afterAll();
         compose.close();
     }
 
@@ -73,8 +74,8 @@ public class RepoTests extends ConfigTests {
     public void checkServices() {
         super.checkTestcontainerComposeService(
              compose,
-             ConfigTests.SERVICE,
-             ConfigTests.SERVICE_PORT
+             Config.SERVICE,
+             Config.SERVICE_PORT
                                               );
     }
 

src/test/java/com/testcontainer/compose/ServiceTests.java
@@ -1,9 +1,10 @@
 package com.testcontainer.compose;
 
 import com.testcontainer.api.entity.Customer;
-import com.testcontainer.api.service.CustomerService;
 import com.testcontainer.api.repository.IRepository;
+import com.testcontainer.api.service.CustomerService;
 import com.testcontainer.api.service.ICustomerService;
+import com.testcontainer.compose.config.Config;
 import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.testcontainers.containers.DockerComposeContainer;
@@ -23,16 +24,14 @@ import java.util.concurrent.TimeoutException;
 
 import static com.testcontainer.databuilder.CustomerBuilder.customerWithName;
 
-public class ServiceTests extends ConfigTests {
+public class ServiceTests extends Config {
 
+  @Container
+  private static final DockerComposeContainer<?> compose = new Config().compose;
   private Customer customer1;
   private Customer customer2;
   private Customer customer3;
   private List<Customer> customerList;
-
-  @Container
-  private static final DockerComposeContainer<?> compose = new ConfigTests().compose;
-
 
   @Autowired
   private IRepository repo;
@@ -41,14 +40,14 @@ public class ServiceTests extends ConfigTests {
 
 
   @BeforeAll
-  static void beforeAll() {
-    ConfigTests.beforeAll();
+  static void setUpAll() {
+    Config.beforeAll();
   }
 
 
   @AfterAll
-  static void afterAll() {
-    ConfigTests.afterAll();
+  static void tearDownAll() {
+    Config.afterAll();
     compose.close();
   }
 
@@ -84,8 +83,8 @@ public class ServiceTests extends ConfigTests {
 
     super.checkTestcontainerComposeService(
          compose,
-         ConfigTests.SERVICE,
-         ConfigTests.SERVICE_PORT
+         Config.SERVICE,
+         Config.SERVICE_PORT
                                           );
   }
 

src/test/java/com/testcontainer/container/ControllerTests.java
@@ -3,12 +3,12 @@ package com.testcontainer.container;
 import com.github.javafaker.Faker;
 import com.testcontainer.api.entity.Customer;
 import com.testcontainer.api.service.ICustomerService;
+import com.testcontainer.container.config.Config;
+import com.testcontainer.container.config.ControllerConfig;
 import io.restassured.http.ContentType;
 import io.restassured.module.webtestclient.RestAssuredWebTestClient;
 import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit.jupiter.EnabledIf;
 import org.springframework.test.web.reactive.server.WebTestClient;
@@ -30,12 +30,11 @@ import static org.hamcrest.CoreMatchers.containsStringIgnoringCase;
 import static org.hamcrest.CoreMatchers.hasItem;
 import static org.hamcrest.Matchers.is;
 import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
 import static org.springframework.http.HttpStatus.*;
 
-@SpringBootTest(webEnvironment = RANDOM_PORT)
-@AutoConfigureWebTestClient
-public class ControllerTests extends ContainerConfig {
+//@SpringBootTest(webEnvironment = RANDOM_PORT)
+//@AutoConfigureWebTestClient
+public class ControllerTests extends ControllerConfig {
 
   final private String enabledTest = "true";
   private List<Customer> customerList;
@@ -55,13 +54,13 @@ public class ControllerTests extends ContainerConfig {
 
   @BeforeAll
   public static void beforeAll() {
-    ConfigTests.beforeAll();
+    Config.beforeAll();
   }
 
 
   @AfterAll
   public static void afterAll() {
-    ConfigTests.afterAll();
+    Config.afterAll();
   }
 
 

src/test/java/com/testcontainer/container/RepoTests.java
@@ -2,6 +2,7 @@ package com.testcontainer.container;
 
 import com.testcontainer.api.entity.Customer;
 import com.testcontainer.api.repository.IRepository;
+import com.testcontainer.container.config.Config;
 import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Lazy;
@@ -22,7 +23,7 @@ import java.util.concurrent.TimeoutException;
 import static com.testcontainer.databuilder.CustomerBuilder.customerWithName;
 import static org.junit.jupiter.api.Assertions.*;
 
-public class RepoTests extends ConfigTests {
+public class RepoTests extends Config {
 
   final private String enabledTest = "true";
   private List<Customer> customerList;
@@ -35,13 +36,13 @@ public class RepoTests extends ConfigTests {
 
   @BeforeAll
   public static void beforeAll() {
-    ConfigTests.beforeAll();
+    Config.beforeAll();
   }
 
 
   @AfterAll
   public static void afterAll() {
-    ConfigTests.afterAll();
+    Config.afterAll();
   }
 
 

src/test/java/com/testcontainer/container/ServiceTests.java
@@ -4,6 +4,7 @@ import com.testcontainer.api.entity.Customer;
 import com.testcontainer.api.service.CustomerService;
 import com.testcontainer.api.repository.IRepository;
 import com.testcontainer.api.service.ICustomerService;
+import com.testcontainer.container.config.Config;
 import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Lazy;
@@ -25,7 +26,7 @@ import static com.testcontainer.databuilder.CustomerBuilder.customerWithName;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 // https://www.baeldung.com/spring-data-mongodb-reactive
-public class ServiceTests extends ConfigTests {
+public class ServiceTests extends Config {
 
   final private String enabledTest = "true";
   private List<Customer> customerList;
@@ -40,13 +41,13 @@ public class ServiceTests extends ConfigTests {
 
   @BeforeAll
   public static void beforeAll() {
-    ConfigTests.beforeAll();
+    Config.beforeAll();
   }
 
 
   @AfterAll
   public static void afterAll() {
-    ConfigTests.afterAll();
+    Config.afterAll();
   }
 
 